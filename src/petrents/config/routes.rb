bbe8612aab8ddd9f3a66b572f7c59320feb90be5
Nikki Waelkens <nikkiwaelkens@Nikkis-MBP.modem>
2020-05-12 09:45:06 +1000

    created petlisting controller and routes

--- src/petrents/config/routes.rb
+++ src/petrents/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get "/", to: "pages#home", as: "root"
+
+  get "/petlistings", to: "petlistings#index"
+  get "petlistings/:id", to: "petlistings#show"
+  get "petlistings/create", to:"petlistings#create"
 end
